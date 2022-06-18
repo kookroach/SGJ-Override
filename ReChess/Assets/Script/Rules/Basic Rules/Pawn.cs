5e7dbda5e66976ad45401473f7774e80b08e6d39
kookroach <lukas.moungos@tum.de>
2022-06-18 02:56:03 +0200

    Update Pawn.cs

--- ReChess/Assets/Script/Rules/Basic Rules/Pawn.cs
+++ ReChess/Assets/Script/Rules/Basic Rules/Pawn.cs
@@ -1,24 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class Pawn : IRule
+public class Pawn : MonoBehaviour, IRule
 {
     private bool hasMoved = false;
+    public int forwardMovement => hasMoved ? 1 : 2;
+    public int lateralMovement = 1;
 
     public bool CanMoveToTarget(Vector2Int target)
     {
+        var pieces = GameManager.Instance.pieces;
+        var key = pieces.FirstOrDefault(x => x.Value == this).Key;
+        
 
-        return true;
+        if (key == null)
+            return false;
+
+        if (target.x - key.x != forwardMovement && target.x - key.x != -forwardMovement)
+            return false;
+
+
+        GameObject objectOnTarget;
+        var isFree = pieces.TryGetValue(target, out objectOnTarget);
+        if (hasMoved)
+        {
+            if (target.y - key.y == lateralMovement || target.y - key.y == -lateralMovement)
+            {
+                if (!isFree && !objectOnTarget.CompareTag(this.tag))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        if (target.y - key.y == 0 && isFree)
+            return true;
+
+        return false;
     }
 
     public bool OnAction(Vector2Int target) 
     {
-        if (!hasMoved)
-        {
+        if (!CanMoveToTarget(target))
+            return false;
 
-        }
+        this.gameObject.transform.position = new Vector3(target.x, gameObject.transform.position.y, target.y);
 
+        if(!hasMoved)
+            hasMoved = true;
         return true;
     }
 
