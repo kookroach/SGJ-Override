ca708e9b2f650091e2a1b9b6174cbd789e184e09
kookroach <lukas.moungos@tum.de>
2022-06-23 22:44:57 +0200

    fix error

--- ReChess/Assets/Script/ScriptSelector.cs
+++ ReChess/Assets/Script/ScriptSelector.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 
 public class ScriptSelector : MonoBehaviour
 {
-    public void pickFirst()
-    {
-        GameManager.Instance.cardSelect = LayoutData.CardSelect.first;
-        GameManager.Instance.PlayerMoves = GameManager.Instance.layoutData.playerMoves.Where(x => x.card == LayoutData.CardSelect.first).ToList();
-        GameManager.Instance.layoutData.card1.SelectCard();
+    //public void pickFirst()
+    //{
+    //    GameManager.Instance.cardSelect = LayoutData.CardSelect.first;
+    //    GameManager.Instance.PlayerMoves = GameManager.Instance.layoutData.playerMoves.Where(x => x.card == LayoutData.CardSelect.first).ToList();
+    //    GameManager.Instance.layoutData.card1.SelectCard();
         
 
-    }
-    public void pickSecond()
-    {
-        GameManager.Instance.cardSelect = LayoutData.CardSelect.second;
-        GameManager.Instance.PlayerMoves = GameManager.Instance.layoutData.playerMoves.Where(x => x.card == LayoutData.CardSelect.second).ToList();
-        GameManager.Instance.layoutData.card2.SelectCard();
-    }
-    public void pickThird()
-    {
-        GameManager.Instance.cardSelect = LayoutData.CardSelect.third;
-        GameManager.Instance.PlayerMoves = GameManager.Instance.layoutData.playerMoves.Where(x => x.card == LayoutData.CardSelect.third).ToList();
-        GameManager.Instance.layoutData.card3.SelectCard();
-    }
+    //}
+    //public void pickSecond()
+    //{
+    //    GameManager.Instance.cardSelect = LayoutData.CardSelect.second;
+    //    GameManager.Instance.PlayerMoves = GameManager.Instance.layoutData.playerMoves.Where(x => x.card == LayoutData.CardSelect.second).ToList();
+    //    GameManager.Instance.layoutData.card2.SelectCard();
+    //}
+    //public void pickThird()
+    //{
+    //    GameManager.Instance.cardSelect = LayoutData.CardSelect.third;
+    //    GameManager.Instance.PlayerMoves = GameManager.Instance.layoutData.playerMoves.Where(x => x.card == LayoutData.CardSelect.third).ToList();
+    //    GameManager.Instance.layoutData.card3.SelectCard();
+    //}
 }
